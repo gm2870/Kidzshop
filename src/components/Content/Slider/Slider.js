731d5716214cb862d0514f398604a26c6f326ea4
gm2870 <alifarzanegan70@gmail.com>
2019-06-01 01:01:35 +0430

    continue working on img slider

--- src/components/Content/Slider/Slider.js
+++ src/components/Content/Slider/Slider.js
@@ -1,14 +1,49 @@
 import React, { Component } from 'react';
 import Grid from '@material-ui/core/Grid';
-import kid5 from '../../../assets/images/kid5.png';
+import imgs from './imgs';
+import TransitionGroup from "react-transition-group/TransitionGroup";
+import CSSTransition from "react-transition-group/CSSTransition";
+
 class Slider extends Component {
+    state={
+        activeIndex:0,
+        appearHome:true,
+        property:imgs.properties[0]
+    }
+    componentDidMount(){
+        let newIndex = null;
+        this.interval = setInterval(() => {
+
+            newIndex = this.state.property.index + 1
+            console.log(this.state.property);
+            this.setState({
+                property:imgs.properties[newIndex]
+            });
+            if(newIndex==3){
+                
+                this.setState({property:imgs.properties[0]});
+            }
+            // console.log(this.state.property);
+
+          }, 2000);
+         
+    }
     render () {
+        const {appearHome,property} = this.state
+
+        let imgItems = 
+        (
+            <TransitionGroup>
+                <CSSTransition  key={property.id} in={appearHome} appear={true} classNames='fade' timeout={2000}>
+                    <img src={property.src} alt={property.alt} />
+                </CSSTransition>
+            </TransitionGroup>
+        )
+     
         return (
             <div className="slider_container">
                 <Grid container direction="row" justify="center"  alignItems="center">
-                    <div className="slider_images">
-                        <img src={kid5} alt="kid5" className="animated_img"/>
-                    </div>
+                    {imgItems}
                     <div className="leftSide_slider">
                         <div className="slider_text">
                             <div className="slider_text_header1 animated_text">تابستان پسرانه</div>
---
+++ src/components/Content/Slider/imgs.js
@@ -0,0 +1,24 @@
+const imgs = {
+    "properties":[
+    {
+        "src":require("../../../assets/images/kid5.png"),
+        "alt":'kid5',
+        "id":"id1",
+        index:0
+    },
+    {
+        "src":require("../../../assets/images/girl.png"),
+        "alt":"girl",
+        "id":"id2",
+        index:1
+    },
+    {
+        "src":require("../../../assets/images/auth_boy.png"),
+        "alt":"boy",
+        "id":"id3",
+        index:2
+    }
+]
+};
+    
+export default imgs ;
